3bdc52a72f092693d202401fb8e18da7ff63d99b
charudatta <78633279+charudatta-urkude@users.noreply.github.com>
2025-03-03 23:18:15 +0530

    Fixed CORS issue for Vercel frontend

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://localhost:5173",
         "http://localhost:3000",
-        "https://your-frontend-domain.com"
+        "https://ai-negotiation-jqzi.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
